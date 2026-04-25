2d88a491a5c9683612351891bbb458b2579823fb
Hinderio <128827870+Hinderio@users.noreply.github.com>
2026-04-25 11:41:00 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Bump the cache name to invalidate old caches when deploying new features (grid overlay etc.)
-const CACHE_NAME = 'fishtrack-v18-tournament-history-fix';
+const CACHE_NAME = 'fishtrack-v19-leaderboard-detail';
 const ASSETS = [
   './',
   './index.html',
